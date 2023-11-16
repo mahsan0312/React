c9d32c63f8f03a6309a7411a5f8fa32444423eae
Muhammad Ahsan <muhammad.ahsan@healthwire.pk>
2023-11-16 21:19:06 +0500

    React

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -1,23 +1,23 @@
-debugger;
 import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Recipes = () => {
+  const params = useParams();
   const navigate = useNavigate();
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    const url = "/api/v1/recipes/index";
+    const url = `/api/v1/show/${params.id}`;
     fetch(url)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         }
         throw new Error("Network response was not ok.");
       })
-      .then((res) => setRecipes(res))
-      .catch(() => navigate("/"));
-  }, []);
+      .then((response) => setRecipe(response))
+      .catch(() => navigate("/recipes"));
+  }, [params.id]);
 
   const allRecipes = recipes.map((recipe, index) => (
     <div key={index} className="col-md-6 col-lg-4">
@@ -36,6 +36,71 @@ const Recipes = () => {
       </div>
     </div>
   ));
+
+  const ingredientList = () => {
+    let ingredientList = "No ingredients available";
+
+    if (recipe.ingredients.length > 0) {
+      ingredientList = recipe.ingredients
+        .split(",")
+        .map((ingredient, index) => (
+          <li key={index} className="list-group-item">
+            {ingredient}
+          </li>
+        ));
+    }
+
+    return ingredientList;
+  };
+
+  const recipeInstruction = addHtmlEntities(recipe.instruction);
+
+  return (
+    <div className="">
+      <div className="hero position-relative d-flex align-items-center justify-content-center">
+        <img
+          src={recipe.image}
+          alt={`${recipe.name} image`}
+          className="img-fluid position-absolute"
+        />
+        <div className="overlay bg-dark position-absolute" />
+        <h1 className="display-4 position-relative text-white">
+          {recipe.name}
+        </h1>
+      </div>
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-sm-12 col-lg-3">
+            <ul className="list-group">
+              <h5 className="mb-2">Ingredients</h5>
+              {ingredientList()}
+            </ul>
+          </div>
+          <div className="col-sm-12 col-lg-7">
+            <h5 className="mb-2">Preparation Instructions</h5>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `${recipeInstruction}`,
+              }}
+            />
+          </div>
+          <div className="col-sm-12 col-lg-2">
+            <button
+              type="button"
+              className="btn btn-danger"
+            >
+              Delete Recipe
+            </button>
+          </div>
+        </div>
+        <Link to="/recipes" className="btn btn-link">
+          Back to recipes
+        </Link>
+      </div>
+    </div>
+    );
+  };
+
   const noRecipe = (
     <div className="vw-100 vh-50 d-flex align-items-center justify-content-center">
       <h4>
@@ -73,6 +138,5 @@ const Recipes = () => {
       </div>
     </>
   );
-};
 
 export default Recipes;
